31d29692d7f4ff81946070c58f3adf22d9461cb3
BradleyHazel <bradleyhazel@gmail.com>
2022-04-16 12:23:01 -0400

    fix

--- backend/connection.js
+++ backend/connection.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://test1234:1234@cluster0.ic7bm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-/*
+const mongoURI = 
   //check if the node environment is production
   process.env.NODE_ENV === "production"
     ? //if so, use DB_URL as the database location
       process.env.DB_URL
     : //if not, use the book-e db on the MongoDB's local server
       "mongodb://localhost/";
-*/
 
 mongoose
   .connect(mongoURI)
