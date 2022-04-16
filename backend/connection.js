7734cb7c9a160c29c94eec5d2971b9bd10e0c991
BradleyHazel <bradleyhazel@gmail.com>
2022-04-16 14:24:40 -0400

    Added Env

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const mongoURI = 
+
   //check if the node environment is production
   process.env.NODE_ENV === "production"
     ? //if so, use DB_URL as the database location
       process.env.DB_URL
     : //if not, use the book-e db on the MongoDB's local server
-      "mongodb://localhost/";
+    process.env.DEV_DB_URL
 
 
 mongoose
