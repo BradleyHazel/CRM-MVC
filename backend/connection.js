9aad4f1ee1f72a89478f35725f1e6abe2a325cd7
BradleyHazel <bradleyhazel@gmail.com>
2022-04-16 12:18:17 -0400

    Added buttons to customer page

--- backend/connection.js
+++ backend/connection.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const mongoURI =
+const mongoURI = "mongodb+srv://test1234:1234@cluster0.ic7bm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+/*
   //check if the node environment is production
   process.env.NODE_ENV === "production"
     ? //if so, use DB_URL as the database location
       process.env.DB_URL
     : //if not, use the book-e db on the MongoDB's local server
       "mongodb://localhost/";
+*/
 
 mongoose
   .connect(mongoURI)
