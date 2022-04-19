57ce20608174e343d24770446162b3c6a00fcd78
BradleyHazel <bradleyhazel@gmail.com>
2022-04-19 07:56:53 -0400

    fix

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
-
-const mongoURI = process.env.DEV_DB_URL
+const mongoURI = 
+  //check if the node environment is production
+  process.env.NODE_ENV === "production"
+    ? //if so, use DB_URL as the database location
+      process.env.DB_URL
+    : //if not, use the book-e db on the MongoDB's local server
+    process.env.DEV_DB_URL
 
 
 mongoose
