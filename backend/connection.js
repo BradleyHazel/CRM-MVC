b7dc6116eea6775aa0dbdf9466fc948293dce020
BradleyHazel <bradleyhazel@gmail.com>
2022-04-19 22:50:36 -0400

    Scaled the charts and added $ sign to y axis

--- backend/connection.js
+++ backend/connection.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://54321:54321@cluster0.ic7bm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-/*
+const mongoURI = 
   //check if the node environment is production
   process.env.NODE_ENV === "production"
     ? //if so, use DB_URL as the database location
       process.env.DB_URL
     : //if not, use the book-e db on the MongoDB's local server
     process.env.DEV_DB_URL
-*/
+
 
 mongoose
   .connect(mongoURI)
