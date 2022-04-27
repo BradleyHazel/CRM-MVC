5ed3058c5a505c413d65cc14445d7d19c90a071b
BradleyHazel <bradleyhazel@gmail.com>
2022-04-26 22:27:42 -0400

    Added a fourth chart, invoice amount per last 12 months

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -22,11 +22,182 @@ router.get("/list", (req, res, next) => {
 
 router.get("/", (req, res, next) => {
 
+
+
   let invoices = [];
   Customer.find({ owner: (req.user._id? req.user._id : req.user.id)  })
     .then((customers) => {
       Invoice.find({ owner: (req.user._id? req.user._id : req.user.id)      }).then((result) => {
         invoices.push(result);
+
+       
+        // need two data sets
+        // months where the last month is the current month
+        // invoice amount per month of the last year
+        let today = new Date();
+        let thisyear = today.getFullYear();
+        thisyear = thisyear.toString();
+        let thismonth = today.getMonth();
+        thismonth = thismonth + 1;
+        thismonth = thismonth.toString();
+
+        let jan = {amount:0,name:"January"};
+        let feb = {amount:0,name:"February"};
+        let mar = {amount:0,name:"March"};
+        let apr = {amount:0,name:"April"};
+        let may = {amount:0,name:"May"};
+        let jun = {amount:0,name:"June"};
+        let jul = {amount:0,name:"July"};
+        let aug = {amount:0,name:"August"};
+        let sep = {amount:0,name:"September"};
+        let oct = {amount:0,name:"October"};
+        let nov = {amount:0,name:"November"};
+        let dec = {amount:0,name:"December"};
+
+       
+
+        for(let c =0;c<result.length;c++){
+          
+          if(result[c].dueDate.slice(0,4) == thisyear){
+        
+            // this year
+            if(parseInt(result[c].dueDate.slice(5,7))<=parseInt(thismonth)){
+              // this month or less
+              if((result[c].dueDate.slice(5,7))=="01"){
+                jan.amount =  jan.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="02"){
+                feb.amount =  feb.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="03"){
+                mar.amount =  mar.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="04"){
+                apr.amount =  apr.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="05"){
+                may.amount =  may.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="06"){
+                jun.amount =  jun.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="07"){
+                jul.amount =  jul.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="08"){
+                aug.amount =  aug.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="09"){
+                sep.amount =  sep.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="10"){
+                oct.amount =  oct.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="11"){
+                nov.amount =  nov.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="11"){
+                dec.amount =  dec.amount+parseInt(result[c].amount)
+              }
+               
+            }
+          }
+          else if(result[c].dueDate.slice(0,4) == (parseInt(thisyear)-1).toString()){
+
+            if(parseInt(result[c].dueDate.slice(5,7))>parseInt(thismonth)){
+              // last year not including the months past 12 months
+       
+
+              if((result[c].dueDate.slice(5,7))=="01"){
+                jan.amount =  jan.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="02"){
+                feb.amount =  feb.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="03"){
+                mar.amount =  mar.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="04"){
+                apr.amount =  apr.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="05"){
+                may.amount =  may.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="06"){
+                jun.amount =  jun.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="07"){
+                jul.amount =  jul.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="08"){
+                aug.amount =  aug.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="09"){
+                sep.amount =  sep.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="10"){
+                oct.amount =  oct.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="11"){
+                nov.amount =  nov.amount+parseInt(result[c].amount)
+              }
+              else if((result[c].dueDate.slice(5,7))=="11"){
+                dec.amount =  dec.amount+parseInt(result[c].amount)
+              }
+           
+            }
+
+          }
+         
+         
+          
+        }
+        let monthArr ;
+
+        if(thismonth == "1"){
+          monthArr = [feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec,jan]
+        }
+        else if(thismonth == "2"){
+          monthArr = [mar,apr,may,jun,jul,aug,sep,oct,nov,dec,jan,feb]
+        }
+        else if(thismonth == "3"){
+          monthArr = [apr,may,jun,jul,aug,sep,oct,nov,dec,jan,feb,mar]
+        }
+        else if(thismonth == "4"){
+          monthArr = [may,jun,jul,aug,sep,oct,nov,dec,jan,feb,mar,apr]
+        }
+        else if(thismonth == "5"){
+          monthArr = [jun,jul,aug,sep,oct,nov,dec,jan,feb,mar,apr,may]
+        }
+        else if(thismonth == "6"){
+          monthArr = [jul,aug,sep,oct,nov,dec,jan,feb,mar,apr,may,jun]
+        }
+        else if(thismonth == "7"){
+          monthArr = [aug,sep,oct,nov,dec,jan,feb,mar,apr,may,jun,jul]
+        }
+        else if(thismonth == "8"){
+          monthArr = [sep,oct,nov,dec,jan,feb,mar,apr,may,jun,jul,aug]
+        }
+        else if(thismonth == "9"){
+          monthArr = [oct,nov,dec,jan,feb,mar,apr,may,jun,jul,aug,sep]
+        }
+        else if(thismonth == "10"){
+          monthArr = [nov,dec,jan,feb,mar,apr,may,jun,jul,aug,sep,oct]
+        }
+        else if(thismonth == "11"){
+          monthArr = [dec,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov]
+        }
+        else if(thismonth == "12"){
+          monthArr = [jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec]
+        }
+
+        let monthNameArr=[]
+        let monthAmountArr=[]
+        for(let i =0;i<monthArr.length;i++){
+          monthNameArr.push(monthArr[i].name)
+          monthAmountArr.push(monthArr[i].amount)
+        }
+
         // loop through customers
         for (let i = 0; i < customers.length; i++) {
           customers[i].invoicenumber = 0;
@@ -66,6 +237,9 @@ router.get("/", (req, res, next) => {
           countryNameArr: countryNameArr,
           countryCountArr: countryCountArr,
           username: (req.user._id? req.user.username : req.user.displayName),
+          monthNameArr:monthNameArr,
+          monthAmountArr:monthAmountArr
+
         };
         res.render(`index`, { data });
       });
@@ -113,7 +287,7 @@ router.get("/:id", (req, res, next) => {
             customers: customers,
             invoices: invoices,
             username: (req.user._id? req.user.username : req.user.displayName),
-            pageid:req.params.id
+            pageid:req.params.id,
           };
           res.render(`customer`, { data });
         }
