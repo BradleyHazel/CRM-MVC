9b4c3a89c2217e4e8d1a2f8694d7cd68d1510d13
BradleyHazel <bradleyhazel@gmail.com>
2022-04-29 08:21:20 -0400

    Added favicon and made security slightly less strict

--- backend/server.js
+++ backend/server.js
@@ -81,7 +81,7 @@ app.use("/invoices", checkAuthentication, invoiceController);
 
 const loginlimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 5, // Limit each IP to 5 requests per `window` (here, per 10 minutes)
+  max: 10, // Limit each IP to 5 requests per `window` (here, per 10 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
